5354d73da65ef6d1c2ab1e24de05cece7aefec91
Frankie Gavin <frankanthonygavin@gmail.com>
2017-01-18 21:52:33 -0800

    add functionality of api call to be logged to console.

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -6,8 +6,10 @@ var Doctor = require('./../js/doctors.js').doctorModule;
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
   $('#submit-button').click(function() {
+    event.preventDefault();
+
     var medicalIssue = $('#aliment').val();
-    currentDoctorObject.getDoctor(medicalIssue);
+    currentDoctorObject.getDoctors(medicalIssue);
 
   });
 });
--- js/doctors.js
+++ js/doctors.js
@@ -5,29 +5,27 @@ var apiKey = require('./../.env').apiKey;
 // }
 
 
-// function Doctor () {
-// }
-//what parameter in the method?
+function Doctor () {
+}
 Doctor.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey);
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)     .then(function(result){
+    // $('#doctor-list').text("<p>The the following doctors can help you</p> " + result.main.name);
+     console.log(result);
+
+      });
 //   console.log("Hi!");
 //   console.log(result);
-// };
-});
-});
+}
 
 
 
-    // .then(function(result)){
-    // })
+
 
 
   //  .then(function(result) {
   //    //PRINTS DATA
   //    //how to find
-  //    $('#doctor-list').text("<p>The the following doctors can help you</p> " + result.main.name);
-  //     console.log(result);
-  //   })
+
   //   //FAIL MESSAGE
   //  .fail(function(error){
   //    $('#doctor-list').text(error.responseJSON.message);
