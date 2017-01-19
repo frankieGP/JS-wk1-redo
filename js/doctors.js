c94aeb99e5088e80dacd28f46a578616547b5642
Frankie Gavin <frankanthonygavin@gmail.com>
2017-01-19 02:01:32 -0800

    add array properly displaying

--- js/doctors.js
+++ js/doctors.js
@@ -8,17 +8,18 @@ function Doctor () {
 }
 Doctor.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)     .then(function(result){
-    // for (var i = 0; i < result.data.length; i ++ 1) {
-      $('#doctor-list').forEach(result.data[i].profile.first_name + " " + result.data[i].profile.last_name);
-    // }
+      $('#doctor-list').text(result.data.forEach(function(data) {
+        $('#doctor-list').append('<p>' + data.profile.first_name + " " + data.profile.last_name + '</p>');
       console.log(result);
+    }));
 
-    //  console.log(result.object[0]data.first_name);
-    //  console.log(result.data[0].profile.first_name + result.data[0].profile.last_name);
-      });
 
-}
+      })
 
+      .fail(function(error){
+         console.log("No matches found. Please try again.");
+       });
+   };
 
 
 
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -13,14 +13,14 @@ var Doctor = require('./../js/doctors.js').doctorModule;
 
 
 $(document).ready(function() {
+  var currentDoctorObject = new Doctor();
+
   $('#submit-button').click(function(event) {
     event.preventDefault();
     var medicalIssue = $('#aliment').val();
-    var currentDoctorObject = new Doctor();
-    results.forEach(function(data) {
-      $(".doctor-list").append(data)
-    // currentDoctorObject.getDoctors(medicalIssue);
-  };
+
+    
+    currentDoctorObject.getDoctors(medicalIssue);
 
   });
 });
